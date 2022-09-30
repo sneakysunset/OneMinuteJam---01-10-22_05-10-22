a8f58ab9ac68d8b74e40a2480574d934fc67fb2e
sneakysunset <andrelauzier@gmail.com>
2022-09-30 11:47:17 +0200

    Anim on Move

--- Assets/Script/MovementManager.cs
+++ Assets/Script/MovementManager.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class MovementManager : MonoBehaviour
 {
+    [Range(.1f, 10)]
+    public float timeForMovement;
+    public AnimationCurve movementAnimCurve;
+
     public void Move(Vector3 destination, Transform player)
     {
+        StartCoroutine(smoothMovement(player.position, destination, player));
 
-        player.position = destination;
     }
 
+    IEnumerator smoothMovement(Vector3 startPos, Vector3 endPos, Transform player)
+    {
+        float i = 0;
+        GridGenerator.Instance.inAnim = true;
+        while(i < 1)
+        {
+            player.position = Vector3.Lerp(startPos, endPos, movementAnimCurve.Evaluate(i));
+
+            i += Time.deltaTime * (1 / timeForMovement);
+            yield return null;
+        }
+        player.position = endPos;
+        GridGenerator.Instance.inAnim = false;
+
+        yield return null;
+    }
 }
